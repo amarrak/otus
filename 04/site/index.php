9fdc8cea9fbd1550fb6003f3ff0cf71b29712cbb
amarrak <68420701+amarrak@users.noreply.github.com>
2020-07-21 11:29:00 +0200

    Порты и пробы

--- 04/site/index.php
+++ 04/site/index.php
@@ -1,11 +1,11 @@
 <?php
-if (strpos($_SERVER["REQUEST_URI"], "/health/") === 0)
+if ((strpos($_SERVER["REQUEST_URI"], "/health/") === 0) || ($_SERVER["REQUEST_URI"] === "/health"))
 {
 	echo '{"status": "OK"}';
 	die();
 }
 
-if (strpos($_SERVER["REQUEST_URI"], "/phpinfo/") === 0)
+if ((strpos($_SERVER["REQUEST_URI"], "/phpinfo/") === 0) || ($_SERVER["REQUEST_URI"] === "/phpinfo"))
 {
 	phpinfo();
 	die();
